b63fec13728f0dcc353ce210d7e0c1cd666581a4
hleao <henrique.leao@softvaro.com.br>
2022-09-13 13:48:22 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+import * as S from './styles/home/styles';
+
+const Home: NextPage = () => {
+  return (
+    <S.Container>
+      <p>Let`s Code</p>
+    </S.Container>
+
+  )
+}
+
+export default Home
